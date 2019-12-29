bbf5999d2f97ff544cefcfeda426249a8f5b127f
MoistSenpai <supercrab115@gmail.com>
2019-12-29 17:23:51 -0500

    Added ping command ready event

--- index.py
+++ index.py
@@ -14,7 +14,25 @@ nep = commands.Bot(
     ownerIds=[184157133187710977, 251091302303662080],
     )
 
+# Ready event
+@nep.event
+async def on_ready():
+    print(f'Client logged in as {nep.user.name}')
 
+# TODO: setup cogs
+# Test command
+@nep.command(name='ping', description='Usual ping-pong command.')
+async def ping(c):
+    await c.send('Pongy pong pong')
+
+@nep.command(name='kill', description='Logs bot out and kills process')
+async def kill(c):
+    if await nep.is_owner(c.message.author) == False:
+        await c.send('Not owner.')
+        return
+    
+    await c.send('Killing...')
+    await nep.logout()
 
 # Login
-nep.login(os.getenv('TOKEN'))
+nep.run(os.getenv('TOKEN'))
