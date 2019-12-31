9b107ba713c554213fdb1658948a110f560f7c8f
MoistSenpai <supercrab115@gmail.com>
2019-12-31 00:39:56 -0500

    Added queue listing

--- Cogs/Music.py
+++ Cogs/Music.py
@@ -14,7 +14,20 @@ class Music(commands.Cog):
     aliases=['q'],
     descrription='Shows the queue')
     async def queue(self, c):
-        pass
+        q = self.nep.util.get_queue(c.guild)._queue
+
+        # Find queue text
+        if len(q) > 0:
+            q_text = [f':dancer: | **{len(q)}** total in the queue:']
+            q_text += [
+                f'{index + 1}) [{item.title}]({item.link}) **[{item.requested_by}]**' for (index, item) in enumerate(q)]
+            
+            # Send queue
+            return await self.nep.util.embed(q_text.join('\n'))
+        
+        # Queue is empty
+        await self.nep.util.embed('<a:WhereTf:539164678480199720> | *You can\'t list anything if there\'s nothing to list*')
+            
 
     # ---------------------------------------------------
 
